ba4b54c174925d1b99203b092eec78738054a47b
Ediglebison <ediglebison@gmail.com>
2021-07-28 02:00:22 -0300

    Terminado....eu acho

--- Locadora.py
+++ Locadora.py
@@ -13,12 +13,13 @@ class Produto():
         self.desconto = 1
         self.desc_Prod = None
         self.produtos = []
+        self.valorFinal = 0
 
     def getProdutos(self):
-        return self.produtos
+        return valorFinal
 
-    def setProdutos(self, produtos):
-        self.produtos = produtos
+    def setProdutos(self, valorFinal):
+        valorFinal = valorFinal + self.getValor()
 
     def getTitulo(self):
         return self.titulo
@@ -63,7 +64,7 @@ class Livro(Produto):
         self.autor = autor
           
     def getDesconto(self):
-        desc = super().desconto * 0.9
+        desc = super().getDesconto() * 0.9
         return desc
 
     def descreve_produto(self, desc_Prod):
@@ -86,7 +87,7 @@ class CD(Produto):
         self.artista = artista
         
     def getDesconto(self):
-        desc = super().desconto * 0.85
+        desc = super().getDesconto() * 0.85
         return desc
 
     def descreve_produto(self, desc_Prod):
@@ -108,7 +109,7 @@ class DVD(Produto):
         self.duracao = duracao
 
     def getDesconto(self):
-        desc = super().desconto * 0.8
+        desc = super().getDesconto() * 0.8
         return desc
         
     def descreve_produto(self, desc_Prod):
@@ -128,20 +129,17 @@ obj_Produto = Produto()
 obj_Livro = Livro()
 obj_CD = CD()
 obj_DVD = DVD()
+valorFinal = []
 
 obj_Produto.setTitulo("Café")
 obj_Produto.setValor(3.00)
 obj_Produto.descreve_produto("Produto muito utilizado por programadores em seu dia a dia")
 obj_Produto.setDesconto(1)
 
-obj_Livro.setTitulo("Redes de Computadores")
-obj_Livro.setValor(149.90)
-obj_Livro.descreve_produto("´Redes de computadores´ foi atualizado para refletir as tecnologias mais novas e \
-                            mais importantes de redes, com ênfase especial em redes sem fio, incluindo 802.11, \
-                            Bluetooth, comunicação sem fio de banda larga, redes ad hoc, i-mode e WAP. Porém, \
-                            as redes fixas não foram ignoradas com cobertura de ADSL, Internet via cabo, Ethernet \
-                            de gigabit, redes não-hierárquicas, NAT e MPLS.")
-obj_Livro.setAutor("Andrew Tanenbaum")
+obj_Livro.setTitulo("O Guia do Mochileiro das Galáxias")
+obj_Livro.setValor(19.90)
+obj_Livro.descreve_produto("42")
+obj_Livro.setAutor("Douglas Adams")
 obj_Livro.setDesconto(1)
 
 obj_CD.setTitulo("CD - Coletanea programação")
@@ -158,11 +156,21 @@ obj_DVD.setDesconto(1)
 
     
 produto1 = [obj_Produto.getTitulo(), obj_Produto.descricao(), obj_Produto.getValor()]
-produto2 = [obj_Livro.getTitulo(), obj_Livro.descricao(), obj_Livro.getAutor(), obj_Livro.getValor()]
-produto3 = [obj_CD.getTitulo(), obj_CD.descricao(), obj_CD.getArtista(), obj_CD.getValor()]
-produto4 = [obj_DVD.getTitulo(), obj_DVD.descricao(), obj_DVD.getDuracao(), obj_DVD.getValor()]
+produto2 = [obj_Livro.getTitulo(), obj_Livro.descricao(), obj_Livro.getValor(), obj_Livro.getAutor()]
+produto3 = [obj_CD.getTitulo(), obj_CD.descricao(), obj_CD.getValor(), obj_CD.getArtista()]
+produto4 = [obj_DVD.getTitulo(), obj_DVD.descricao(), obj_DVD.getValor(), obj_DVD.getDuracao()]
 
 print (produto1)
 print (produto2)
 print (produto3)
 print (produto4)
+
+valorFinal.append(produto1[2])
+valorFinal.append(produto2[2])
+valorFinal.append(produto3[2])
+valorFinal.append(produto4[2])
+
+valorFinal = sum(valorFinal)
+
+
+print("Valor total: ",valorFinal)
